2cfee53ff1ee2e5ba4ec8f4b5ac11099d7c25c07
Joel Juntunen <joksujun@gmail.com>
2023-10-05 14:45:23 +0300

    Page Param

---
+++ PizzeriaProjekti/PizzeriaProjekti/Authentication/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,16 @@
+﻿/*
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using System.Security.Claims;
+
+namespace PizzeriaProjekti.Authentication
+{
+    public class CustomAuthenticationStateProvider
+    {
+        private readonly ProtectedSessionStorage _sessionStorage;
+        private ClaimsPrincipal _anonymous = new ClaimsPricipal(new ClaimsIdentity());
+
+        public CustomAuthentication
+    }
+}
+*/
