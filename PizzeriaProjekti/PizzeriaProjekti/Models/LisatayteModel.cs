bbef741f2dcbf3d915905c91ce0dac7e0474fefd
Joel Juntunen <joksujun@gmail.com>
2023-10-06 13:32:48 +0300

    lisätäyte model

---
+++ PizzeriaProjekti/PizzeriaProjekti/Models/LisatayteModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzeriaProjekti.Models
+{
+    public class LisatayteModel
+    {
+        public int Lisatayte_ID { get; set; }
+        public string LT_Nimi { get; set; }
+        public float LT_Hinta { get; set; }
+    }
+}
